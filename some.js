6ccfa3d006403f49ce50b5907fe9bb0fb1442bb2
Farit Vinogradov <fora94@mail.ru>
2013-03-15 17:10:09 +0400

    -add all names for tooltip

--- some.js
+++ some.js
@@ -3,32 +3,26 @@
 {header:"Футбольное поле", text:"Футбольное поле на 10-12 человек."},
 {header:"Лесная площадка", text:"В центре расположен корабль «Черная жемчужина», а по периметру в хаотичном порядке располагаются различные строения — укрытия."},
 {header:"Главная поляна", text:"Место для проведения различных сценарных и сюжетно-приключенческих игр."},
-{header:"Cake powder", text:"Tart tart applicake ice cream gummi bears jujubes jujubes. Applicake marshmallow marzipan jelly sweet."},
-{header:"Tart", text:"Brownie powder powder carrot cake lollipop souffle. Halvah lemon drops jelly chocolate cake tiramisu dessert tootsie roll sweet."},
-{header:"Croissant", text:"Cupcake applicake applicake pie lollipop pastry caramels oat cake sweet. "},
-{header:"Jelly-o macaroon", text:"Croissant jelly beans gummi bears lemon drops cheesecake tootsie roll tootsie roll muffin powder."},
-{header:"Lollipop", text:"Jelly-o macaroon cake brownie. Halvah applicake dessert marshmallow pastry."},
-{header:"Apple pie", text:"Halvah applicake fruitcake candy sesame snaps gummi bears jelly-o brownie."},
-{header:"Pastry", text:"Chocolate toffee wafer jujubes brownie wypas halvah toffee. Sweet croissant sugar plum danish candy cookie powder topping. "},
-{header:"Chocolate toffee", text:"Dessert tiramisu pie dragee tart gummies danish. Cookie brownie gummi bears toffee sesame snaps chupa chups marzipan jelly candy."},
-{header:"Chocolate bar", text:"Jelly halvah jelly beans brownie tart. Marshmallow biscuit cookie sweet jelly cookie powder candy pastry."},
-{header:"Pudding", text:"Cupcake sugar plum tootsie roll cookie cookie chocolate bar dessert chocolate cake. Jujubes macaroon icing sugar plum dragee pie."},
-{header:"Jelly halvah", text:"Pudding gummies candy chupa chups jujubes bear claw sugar plum pastry carrot cake. Topping sesame snaps lollipop gummies jujubes liquorice cookie cookie."},
-{header:"Jujubes", text:"Chupa chups macaroon candy brownie faworki donut toffee cheesecake pastry. "},
-{header:"Cake powder", text:"Wypas marshmallow bonbon croissant chocolate bar. Biscuit oat cake jelly-o marshmallow faworki chupa chups jujubes."},
-{header:"Chupa chups", text:"Lollipop apple pie tootsie roll. Chocolate cake liquorice cotton candy tootsie roll cupcake tootsie roll cookie ice cream. "},
-{header:"Applicake marshmallow", text:"Cupcake gummi bears pastry jelly jujubes liquorice marshmallow oat cake pastry. Cake powder tiramisu. "},
-{header:"Bear claw", text:"Sesame snaps pudding cookie cookie tootsie roll jelly jelly-o. Chocolate bonbon wypas."},
-{header:"Sesame snaps", text:"Pastry macaroon pie jelly beans oat cake pie fruitcake. Jelly marshmallow wypas macaroon."},
-{header:"Chocolate toffee", text:"Chocolate bar fruitcake pudding. Icing lemon drops tootsie roll."},
-{header:"Lollipop apple", text:"Cheesecake marshmallow caramels cake lollipop."}
+{header:"Скалодром", text:"9-метровый скалодром, оборудованный стационарными верёвками и страховочными устройствами."},
+{header:"Высотный город", text:"Уникальный комплекс высотных заданий-препятствий на высоте свыше 6 метров над землей."},
+{header:"Троллея", text:"Аналог парашютной вышки или тарзанки. "},
+{header:"Парковка", text:""},
+{header:"Домик", text:"Теплый домик на команию 20 человек."},
+{header:"Домик", text:"Теплый домик на 6-8 человек."},
+{header:"Домик", text:"Самое большое отапливаемое помещение в Солдате Удачи на 100 человек. "},
+{header:"Служебное помещение", text:""},
+{header:"Баня", text:" Березовые венечки, теплый предбанник с уютной обстановкой, а самое главное теплая атмосфера."},
+{header:"Терраса", text:"Помещение на компанию 30-40 человек."},
+{header:"Охотничий домик", text:"Первый этаж - комната с камином на 20 человек, мансарда на 25-30 человек."},
+{header:"Беседка", text:"Для любителей природы и свежего воздуха мы предоставляем комфортные беседки."},
+{header:"Беседка", text:"Для любителей природы и свежего воздуха мы предоставляем комфортные беседки."},
+{header:"Домик", text:"Избушка на курьих ножках для компании из 10 человек. "}
 ];
 
 $(document).ready(function() {
 	Planstery.bind("onselect" , function(id){
 		var mainContainer = $("#planstery-main-container");
-		clr();
-		toolTip(cubique[id[0].id - 1].header, cubique[id[0].id -1].text, id[0], cubique[id[0].id-1].img);
+		toolTip(cubique[id[0].id - 1].header, cubique[id[0].id -1].text, id[0]);
 	});
 	Planstery.bind("ondeselect" , function(){
 		toolTip();
@@ -69,6 +63,7 @@ function toolTip(header, text, object, img) {
 	var tooltipText = $("#planstery-tooltip-text");
 	w = 150; // Tooltip's wigth
 	var mainContainer = $("#planstery-main-container");
+	var str = "Free Web Building Tutorials!";
 	if (header) {
 		var x = mainContainer.offset().left + object.bounds.center.x;
 		var y = mainContainer.offset().top + object.bounds.center.y - object.bounds.height/2 - 20;
@@ -80,8 +75,9 @@ function toolTip(header, text, object, img) {
 			"top": y - tooltip.height() + 'px'
 		})
 		if(text){
-			tooltipText.text(text);
+			//tooltipText.text(text+'\n'+str.link("http://www.w3schools.com"));
 			tooltipText.css({"display":"block"})
+			//document.write(str.link("http://www.w3schools.com"));
 //			if(img){
 					
 //				}
